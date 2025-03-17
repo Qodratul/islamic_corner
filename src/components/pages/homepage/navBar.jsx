e54e128f290b0e81327c17d2f7319c268bf0bd26
Qodratul <mqudri2005@gmail.com>
2025-03-17 10:43:36 +0700

    Adding Logo

--- src/components/pages/homepage/navBar.jsx
+++ src/components/pages/homepage/navBar.jsx
@@ -1,20 +1,27 @@
 import "../../../App.css";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const NavBar = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <>
-        <div className=" w-full bg-[#8ba378] p-2 flex justify-between items-center shadow-md z-10">
-            <span className="text-[2.5rem] font-bold text-center flex items-center cursor-pointer" onClick={() => {
-                navigate(`/`);
-            }}>
-                Islamic Corner
-            </span>
-        </div>
+  return (
+    <>
+      <div className=" w-full bg-[#8ba378] p-2 flex justify-between items-center shadow-md z-10">
+        <span
+          className="text-[2.5rem] font-bold text-center flex items-center cursor-pointer"
+          onClick={() => {
+            navigate(`/`);
+          }}
+        >
+          <img
+            src={"../../../../public/icons/whitelogo.svg"}
+            className={"h-10 w-10 mr-1"}
+          />
+          Islamic Corner
+        </span>
+      </div>
     </>
-    );
+  );
 };
 
-export default NavBar
+export default NavBar;
